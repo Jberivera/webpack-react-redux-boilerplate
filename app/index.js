b15f4a7b487d22414920f4efaeac1c08c6dc3b1b
Juan Bernardo <jrivera@hugeinc.com>
2016-05-07 10:44:41 -0500

    middlerwares refactor

--- app/index.js
+++ app/index.js
@@ -2,19 +2,40 @@ import './main.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+import createLogger from 'redux-logger';
 
 // reducers
-import myAppStore from './reducers/myApp';
+import reducers from './reducers';
+
+const reducer = combineReducers(Object.assign({}, reducers, {
+  routing: routerReducer
+}));
+
+const store = createStore(
+  reducer,
+  applyMiddleware(
+    thunk,
+    createLogger()
+  )
+);
+
+const history = syncHistoryWithStore(browserHistory, store);
 
 // react components
-import ShowNote from './containers/ShowNote';
+import { Note } from './components';
 
 const App = (props) => (
-  <Provider store={createStore(myAppStore)}>
-    <ShowNote />
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Note}>
+      </Route>
+    </Router>
   </Provider>
-)
+);
 
 ReactDOM.render(<App />, document.querySelector('#app'));
