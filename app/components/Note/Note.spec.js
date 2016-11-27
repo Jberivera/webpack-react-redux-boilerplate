f700cf851fb7abf7525411bc9e462bd60968b540
Juan Bernardo <jrivera@hugeinc.com>
2016-11-26 23:25:49 -0500

    Package Update

--- app/components/Note/Note.spec.js
+++ app/components/Note/Note.spec.js
@@ -6,14 +6,16 @@ import { shallow } from 'enzyme';
 describe('<Note />', () => {
   it('has a h1 element', () => {
     const wrapper = shallow(<Note />);
+
     expect(wrapper.find('h1').length).toBe(1);
   });
 
   it('simulates click events', () => {
     const onButtonClick = createSpy();
     const wrapper = shallow(
-      <Note increment={onButtonClick} />
+      <Note increment={ onButtonClick } />
     );
+
     wrapper.find('h1').simulate('click');
     expect(onButtonClick).toHaveBeenCalled();
   });
